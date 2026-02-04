bd3f1df1ab94439af5d79211567743705160a246
Felipe silva <lipesx07@gmail.com>
2026-02-04 08:03:06 -0300

    feat: adding cpf validation

---
+++ src/utils/cpf-validation.ts
@@ -0,0 +1,28 @@
+export function cpfValidate(cpf: string): boolean {
+    const cleanCpf = cpf.replace(/\D/g, '')
+
+    if (cleanCpf.length !== 11) return false
+    if(/^(\d)\1{10}$/.test(cleanCpf)) return false
+
+    let sum = 0
+    let rest: number
+
+    for (let i = 0; i < 9; i++ ) {
+        sum += Number(cleanCpf[i]) * (10 - i)
+    }
+
+    rest = (sum * 10) % 11
+    if (rest === 10 || rest === 11) rest = 0
+    if (rest !== Number(cleanCpf[9])) return false
+
+    sum = 0
+    for (let i = 0; i < 10; i++) { 
+        sum += Number(cleanCpf[i]) * (11 - i)
+    }
+
+    rest = (sum * 10) % 11
+    if (rest === 10 || rest === 11) rest = 0
+    if (rest !== Number(cleanCpf[10])) return false
+
+    return true
+}
