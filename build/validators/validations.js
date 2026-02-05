dcddab4e3478bb3fa9f150f044dadb177114bd2e
Felipe silva <lipesx07@gmail.com>
2026-02-05 16:15:16 -0300

    refactor: converting message variables to English

--- build/validators/validations.js
+++ build/validators/validations.js
@@ -3,15 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.idSchema = exports.CreateRequestSchema = void 0;
 const zod_1 = require("zod");
 const validationsMessages = {
-    Endereco: {
+    Address: {
         Required: "Endereço é obrigatório",
         Min: "Endereço deve ter pelo menos 5 caracteres",
         Max: "Endereço inválido"
     },
-    PontoReferencia: {
+    Reference: {
         Max: "Ponto de referência muito longo (máximo 200 caracteres)"
     },
-    Descricao: {
+    Description: {
         Required: "Descrição é obrigatória",
         Min: "Descreva o ocorrido com pelo menos 10 caracteres",
         Max: "Descrição muito longa (máximo 1000 caracteres)"
@@ -19,15 +19,15 @@ const validationsMessages = {
 };
 exports.CreateRequestSchema = zod_1.z.object({
     address: zod_1.z.string()
-        .min(1, validationsMessages.Endereco.Required)
-        .min(5, validationsMessages.Endereco.Min)
-        .max(300, validationsMessages.Endereco.Max),
+        .min(1, validationsMessages.Address.Required)
+        .min(5, validationsMessages.Address.Min)
+        .max(300, validationsMessages.Address.Max),
     reference: zod_1.z.string()
         .optional(),
     description: zod_1.z.string()
-        .min(1, validationsMessages.Descricao.Required)
-        .min(10, validationsMessages.Descricao.Min)
-        .max(1000, validationsMessages.Descricao.Max),
+        .min(1, validationsMessages.Description.Required)
+        .min(10, validationsMessages.Description.Min)
+        .max(1000, validationsMessages.Description.Max),
 });
 exports.idSchema = zod_1.z.object({
     id: zod_1.z.string().uuid(),
